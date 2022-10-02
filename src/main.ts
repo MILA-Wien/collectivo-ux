01bf5bf4717f11ed53825bb5b4f93c2edb48854d
Leon <l.beccard@protonmail.com>
2022-10-02 16:50:32 +0200

    Add first button for login page

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { setupI18n, loadLocaleMessages, setI18nLanguage } from "@/locales/i18n";
+import PrimeVue from "primevue/config";
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
+
 
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+import "primevue/resources/themes/saga-blue/theme.css"       //theme
+import "primevue/resources/primevue.min.css"                 //core css
+import "primeicons/primeicons.css"                           //icons
 
 const app = createApp(App);
 const i18n = setupI18n();
@@ -16,5 +23,8 @@ setI18nLanguage(i18n, "de");
 app.use(i18n);
 app.use(createPinia());
 app.use(router);
+app.use(PrimeVue);
+app.component('Dialog', Dialog);
+app.component('Button', Button);
 
 app.mount("#app");
---
+++ src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+    <div>
+        <Button label="Hello"></Button>
+    </div>
+
+</template>
+
+<style>
+
+</style>
+  
