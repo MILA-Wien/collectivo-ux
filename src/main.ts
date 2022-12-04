7f37b8a8bc57dd5d635eb103e2f6e0f4f1883d4b
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2022-12-04 13:37:03 +0100

    Order imports

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { initKeycloak } from "./helpers/keycloak";
 import { setupI18n, loadLocaleMessages, setI18nLanguage } from "@/locales/i18n";
-
 import App from "./App.vue";
 import router from "./router";
-
 import "./assets/main.css";
 
 //PrimeVue imports
@@ -15,21 +14,28 @@ import PasswordPrime from "primevue/password";
 import Menu from "primevue/menu";
 import ToastService from "primevue/toastservice";
 import Toast from "primevue/toast";
-
 import "primevue/resources/themes/saga-blue/theme.css"; //theme
 import "primevue/resources/primevue.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
-import { initKeycloak } from "./helpers/keycloak";
-// init keycloak
+
+// init vue app
 const i18n = setupI18n();
 const app = createApp(App);
 app.use(createPinia());
+
+// init keycloak
 const keycloakInstance = initKeycloak();
 keycloakInstance
   .then((keycloak) => {
     console.log("keycloakInstance", keycloak);
+
+    // init translations
     app.use(i18n);
-    // PrimeVue
+    loadLocaleMessages(i18n, "en");
+    loadLocaleMessages(i18n, "de");
+    setI18nLanguage(i18n, "de");
+
+    // init PrimeVue
     app.use(PrimeVue);
     app.use(ToastService);
     app.component("InputTextPrime", InputTextPrime);
@@ -38,12 +44,10 @@ keycloakInstance
     app.component("PrimeMenu", Menu);
     app.component("ToastPrime", Toast);
 
-    // init translations
-    loadLocaleMessages(i18n, "en");
-    loadLocaleMessages(i18n, "de");
-    setI18nLanguage(i18n, "de");
-
+    // init view router
     app.use(router);
+
+    // Render app
     app.mount("#app");
   })
   .catch((e) => {
