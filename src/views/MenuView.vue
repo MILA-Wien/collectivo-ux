927647f50e37429d35f45c3fd597637ccb3999ad
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2022-12-07 16:29:41 +0100

    Change to correct logo

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -10,14 +10,14 @@
 
     <div
       :class="isOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'"
-      class="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition
+      class="fixed inset-y-0 left-0 z-30 w-60 overflow-y-auto transition
       duration-300 mr-5 transform bg-white lg:translate-x-0 lg:static lg:inset-0"
     >
       <div class="flex flex-col h-full items-center">
-        <img src="../assets/mila_logo_header.png" alt="MILA Logo" class="p-4">
+        <img src="../assets/mila_logo_subline.png" alt="MILA Logo" class="md:w-2/3 w-1/2 pt-10 pb-4">
         <MenuMain />
         <span class="grow"></span>
-        <span class="my-3 text-sm">collectivo — <VersionItem /></span>
+        <span class="my-3 pl-4 text-sm">collectivo — <VersionItem /></span>
       </div>
     </div>
   </div>
