f500e01956ace48dee824c256f31ad4205b709d5
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2022-12-07 14:36:05 +0100

    Add sidebar margin

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -11,7 +11,7 @@
     <div
       :class="isOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'"
       class="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition
-      duration-300 transform bg-white lg:translate-x-0 lg:static lg:inset-0"
+      duration-300 mr-5 transform bg-white lg:translate-x-0 lg:static lg:inset-0"
     >
       <div class="flex flex-col items-center">
         <img src="../assets/mila_logo_header.png" alt="MILA Logo" class="p-4">
