47fcd34833ef48d95c0d95eb48ff3e18d6fe82be
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2022-12-08 14:22:57 +0100

    Set content lef-bound

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ const { user } = storeToRefs(userStore);
     <div class="flex-1 flex flex-col overflow-hidden">
       <HeaderView />
       <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
-        <div class="container mx-auto px-6 py-4">
+        <div class="container mr-auto px-6 py-4">
           <RouterView />
         </div>
       </main>
