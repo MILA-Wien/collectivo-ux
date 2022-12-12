844fc8d022e14b12123e48de3e23a6c7f3557769
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2022-12-12 14:02:07 +0100

    Update 03_login_verify_email.cy.ts

--- cypress/e2e/03_login_verify_email.cy.ts
+++ cypress/e2e/03_login_verify_email.cy.ts
@@ -7,7 +7,7 @@ describe("Login Succesfull", () => {
       cy.visit("/");
       cy.contains("h1", "Bei Ihrem Konto anmelden");
       cy.get("input[name=username]").type("test_user_not_verified@example.com");
-      cy.get("input[name=password]").type("test").type("{enter}"); // '{enter}' submits the form
+      cy.get("input[name=password]").type("Test123!").type("{enter}"); // '{enter}' submits the form
       cy.contains(
         ".pf-c-alert__title",
         "Sie müssen Ihre E-Mail-Adresse verifizieren, um das Benutzerkonto zu aktivieren."
