8055dc8f1ce161e3cd968eaab434bcbc27afca07
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2022-12-12 12:54:19 +0100

    Remove comment

--- cypress/e2e/02_login_success.cy.ts
+++ cypress/e2e/02_login_success.cy.ts
@@ -8,7 +8,6 @@ describe("Login Succesfull", () => {
       cy.contains("h1", "Bei Ihrem Konto anmelden");
       cy.get("input[name=username]").type("test_member_01@example.com");
       cy.get("input[name=password]").type("test").type("{enter}"); // '{enter}' submits the form
-      // cy.get("#kc-accept").click();
       cy.get('button[id="toggle-sidebar-button"]').click();
       cy.get('#main_menu_0_0 > .p-menuitem-content > .p-menuitem-link > .p-menuitem-text').contains("Startseite");
       cy.get("a").contains("Abmelden").click();
