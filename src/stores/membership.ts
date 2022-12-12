be6d70e4add518731281b122734a3054cbf9da93
Joël Foramitti <57440945+JoelForamitti@users.noreply.github.com>
2022-12-12 15:10:28 +0100

    Improve profile (#22)
    * Add translation and toasts
    
    * Let error propagate
    
    * Add request id to error toast
    
    * Update 02_login_success.cy.ts
    
    * Update 03_login_verify_email.cy.ts
    
    * add run command to ci
    
    * entrypoint instead of entry-point
    
    * fix typo
    
    Co-authored-by: aaron@nanu-c.org <aaron@nan-c.org>

--- src/stores/membership.ts
+++ src/stores/membership.ts
@@ -18,12 +18,7 @@ export const useMembershipStore = defineStore({
   actions: {
     async updateMembership(membership: Member) {
       await updateMembershipFn(membership)
-        .then(() => {
-          this.membership = membership;
-        })
-        .catch((error) => {
-          console.log("put membership error", error);
-        });
+      this.membership = membership;
     },
     async getMembership() {
       await getMembershipFn().then((response) => {
