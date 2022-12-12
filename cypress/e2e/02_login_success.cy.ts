2ce7f3266cb556cc610a8f479cce9c9eb859c379
aaron@nanu-c.org <aaron@nan-c.org>
2022-12-12 13:02:46 +0100

    e2e: fix test password

--- cypress/e2e/02_login_success.cy.ts
+++ cypress/e2e/02_login_success.cy.ts
@@ -7,7 +7,7 @@ describe("Login Succesfull", () => {
       cy.visit("/");
       cy.contains("h1", "Bei Ihrem Konto anmelden");
       cy.get("input[name=username]").type("test_member_01@example.com");
-      cy.get("input[name=password]").type("test").type("{enter}"); // '{enter}' submits the form
+      cy.get("input[name=password]").type("Test123!").type("{enter}"); // '{enter}' submits the form
       cy.get('button[id="toggle-sidebar-button"]').click();
       cy.get('#main_menu_0_0 > .p-menuitem-content > .p-menuitem-link > .p-menuitem-text').contains("Startseite");
       cy.get("a").contains("Abmelden").click();
