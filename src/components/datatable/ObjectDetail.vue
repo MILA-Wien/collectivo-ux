f989677b5a39179fb195ed9c61ada10ed8415deb
Joël Foramitti <57440945+JoelForamitti@users.noreply.github.com>
2023-01-17 16:36:47 +0100

    Update ObjectDetail.vue

--- src/components/datatable/ObjectDetail.vue
+++ src/components/datatable/ObjectDetail.vue
@@ -12,6 +12,9 @@ import type { endpoints } from "@/api/api";
 const { t } = useI18n();
 const emit = defineEmits(["change", "close"]);
 
+const confirm = useConfirm();
+const toast = useToast();
+
 const props = defineProps({
   store: {
     type: Object as PropType<StoreGeneric>,
@@ -35,31 +38,28 @@ const props = defineProps({
   },
 });
 
-const toast = useToast();
-const successToast = () => {
-  toast.add({
-    severity: "success",
-    summary: "Success",
-    detail: props.name + " has been updated.",
-    life: 5000,
-  });
+const successToast = (message: String) => {
+  toast.add({severity: "success", summary: "Success", 
+    detail: message, life: 5000});
 };
 const errorToast = (e: any) => {
-  toast.add({
-    severity: "error",
-    summary: "Error",
-    detail: `Update failed. Request-id: "${
-      e?.response?.headers["x-request-id"]
-    }" Response: "${JSON.stringify(e?.response?.data)}"`,
+  toast.add({severity: "error", summary: "Error",
+    detail: `Update failed. Request-id: "${e?.response?.headers["x-request-id"]}
+      " Response: "${JSON.stringify(e?.response?.data)}"`,
   });
 };
 
 // Create temporary copy of the object
 const object_temp = ref(JSON.parse(JSON.stringify(props.object)));
 
-const displayModal = ref(true);
-const isSaving = ref(false);
+// Delete id from schema so it doesn't show up in the form
 delete props.schema.id
+
+// Reactive settings
+const isVisible = ref(true);
+const isSaving = ref(false);
+
+// Functions
 function closeModal() {
   emit("close");
 }
@@ -69,16 +69,15 @@ async function createObject() {
       await props.store.create(
         props.name,
         object_temp.value);
-    
-    successToast();
+    successToast("Object has been created.");
+    emit("close");
   } catch (error) {
     console.log(error)
     errorToast(error);
   }
-  emit("close");
   isSaving.value = false;
 }
-async function saveObject() {
+async function updateObject() {
   console.log(object_temp.value)
   isSaving.value = true;
     try {
@@ -86,13 +85,12 @@ async function saveObject() {
         props.name,
         object_temp.value['id'],
         object_temp.value);
-
-    successToast();
+    successToast("Object has been updated.");
+    emit("close");
   } catch (error) {
     console.log(error)
     errorToast(error);
   }
-  emit("close");
   isSaving.value = false;
 }
 async function deleteObject() {
@@ -101,7 +99,7 @@ async function deleteObject() {
       await props.store.delete(
         props.name,
         object_temp.value['id']);
-    successToast();
+    successToast("Object has been deleted.");
   } catch (error) {
     console.log(error)
     errorToast(error);
@@ -109,8 +107,6 @@ async function deleteObject() {
   emit("close");
   isSaving.value = false;
 }
-
-const confirm = useConfirm();
 const confirmDelete = () => {
   confirm.require({
     message: 'Are you sure you want to delete this object?',
@@ -119,57 +115,73 @@ const confirmDelete = () => {
     accept: () => {deleteObject()},
   })
 };
-
 </script>
+
 <template>
   <div>
     <ConfirmDialog></ConfirmDialog>
     <Dialog
       :header="t(name) + ' ' + object.id"
-      v-model:visible="displayModal"
+      v-model:visible="isVisible"
       :breakpoints="{ '960px': '75vw', '640px': '90vw' }"
       :style="{ width: '80vw' }"
       :modal="true"
       @hide="closeModal"
     >
       <div class="modal-content">
-        <div
-          v-for="(field, name, index) in schema"
-          :key="name" class="field"
-        >
 
-          <label for="attr-{{name}}">{{ field.label }}</label>
-          <div v-if="field.input_type === 'select'">
-            <Dropdown 
-              v-model="object_temp[name]" 
-              :options="field.options" 
-              optionLabel="label" 
-              optionValue="value"
-              :filter="true"
-              :disabled="field.read_only"
-              placeholder="Select a choice" />
+        <!-- Read only fields -->
+        <div v-for="(field, name, index) in schema" :key="name" class="field">
+          <div v-if="field.read_only">
+            <span class="font-bold">{{ field.label }}</span>: {{ object_temp[name] }}
           </div>
-          <div v-else-if="field.input_type === 'html'">
-            TODO CHANGE THIS TO A HTML EDIT FIELD<br/>
-            <InputText
-              id="attr-{{name}}"
-              type="text"
-              aria-describedby="attr-{{value}}-help"
-              v-model="object_temp[name]"
-              :disabled="field.read_only"
-            />
-          </div>
-          <div v-else>
-            <InputText
-              id="attr-{{name}}"
-              type="text"
-              aria-describedby="attr-{{value}}-help"
-              v-model="object_temp[name]"
-              :disabled="field.read_only"
-            />
-          </div>
-          <small v-if="field.help_text" id="user-attr-{{value}}-help" class="p-info">{{ field.help_text }}</small>
         </div>
+
+        <!-- Editable fields -->
+        <div v-for="(field, name, index) in schema" :key="name" class="field">
+          <div v-if="!field.read_only">
+            <label for="attr-{{name}}">
+              {{ field.label }} 
+              <span v-if="field.required" class="text-red-600">*</span>
+            </label>
+
+            <div v-if="field.input_type === 'select'">
+              <Dropdown 
+                v-model="object_temp[name]" 
+                :options="field.options" 
+                optionLabel="label" 
+                optionValue="value"
+                :filter="true"
+                :disabled="field.read_only"
+                placeholder="Select a choice" />
+            </div>
+
+            <div v-else-if="field.input_type === 'html'">
+              TODO CHANGE THIS TO A HTML EDIT FIELD<br/>
+              <InputText
+                id="attr-{{name}}"
+                type="text"
+                aria-describedby="attr-{{value}}-help"
+                v-model="object_temp[name]"
+                :disabled="field.read_only"
+              />
+            </div>
+
+            <div v-else>
+              <InputText
+                id="attr-{{name}}"
+                type="text"
+                aria-describedby="attr-{{value}}-help"
+                v-model="object_temp[name]"
+                :disabled="field.read_only"
+              />
+            </div>
+
+            <small v-if="field.help_text" id="user-attr-{{value}}-help" class="p-info">{{ field.help_text }}</small>
+          
+          </div>
+        </div>
+        
       </div>
       <template #footer>
         <ButtonPrime
@@ -185,9 +197,8 @@ const confirmDelete = () => {
           :label="t('Save')"
           :loading="isSaving"
           icon="pi pi-check"
-          @click="saveObject()"
+          @click="updateObject()"
         />
-        
         <ButtonPrime
           :label="t('Cancel')"
           icon="pi pi-times"
