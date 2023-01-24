077ed4ec0287995a2e0b45377c2b2091e53fe57e
Riccardo Wasserman <55874538+r-wasserman@users.noreply.github.com>
2023-01-24 16:28:17 +0100

    (+) custom cy.login command (#44)
    * (+) custom cy.login command
    
    * (c) index.d.ts:
    Subject is defined but never used. First try

--- cypress/e2e/05_open_members_table.cy.ts
+++ cypress/e2e/05_open_members_table.cy.ts
@@ -3,11 +3,7 @@
 Cypress.Cookies.debug(true);
 describe("Login Succesfull", () => {
   it("visits the app root url", () => {
-    cy.session("test", () => {
-      cy.visit("/");
-      cy.contains("h1", "Bei Ihrem Konto anmelden");
-      cy.get("input[name=username]").type("test_superuser@example.com");
-      cy.get("input[name=password]").type("Test123!").type("{enter}"); // '{enter}' submits the form
+      cy.login('test_superuser@example.com');
       cy.get('button[id="toggle-sidebar-button"]').click();
       cy.get(
         "#main_menu_0_0 > .p-menuitem-content > .p-menuitem-link > .p-menuitem-text"
@@ -17,6 +13,5 @@ describe("Login Succesfull", () => {
         .click();
       cy.get('div[id="collectivo-backdrop"]').click();
       cy.get('div[class="members-table"').should("exist");
-    });
   });
 });
