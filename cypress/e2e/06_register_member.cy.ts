839bd466c2cddabeb4fab98133e630500f278cd1
Riccardo Wasserman <riccardo.wasserman@gmail.com>
2023-01-30 16:32:30 +0100

    (+) 06_register_member.cy.ts: - first working test case for natural person. - beforeEach() for test data cleanup - readme and todos (c) DashboardView.vue: unique id for welcome <span></span>

--- cypress/e2e/06_register_member.cy.ts
+++ cypress/e2e/06_register_member.cy.ts
@@ -1,10 +1,64 @@
-// https://docs.cypress.io/api/introduction/api.html
+/* 
+README && TODOs:
+1. BUG(?): whenever a new user_not_member registers as member, AND after successfully deleting member id from db (see beforeEach() hook), on new login the UI will display user_as_member StartPage:
+  - StartPage EXPECTED: user_not_member
+  - StartPage ACTUAL: user_as_member
+  Workaround: docker compose restart backend services (@Joel, Keycloak matter)
 
-//TODOs: adapt ids in registration form
+2. JSON response validation on submit formular, 2 possible approaches:
+  2a: deep.equal comparison between actualResponse and expectedResponse 
+      Manipulation of expectedResponse properties necessary, i.e. they need to be made dynamic: "id" (i), "membership_start" (ii) etc.
+  2b: validation of specific actualResponse properties only. Which ones?
+
+3. it("register as legal person", () => {...}
+4. Use cy.fixture() to load test data -> https://docs.cypress.io/api/commands/fixture
+*/
 
 Cypress.Cookies.debug(true);
-describe("register user not member filling out only compulsory fields and checkboxes on page 4/5", () => {
-  it("visits the app root url", () => {
+describe("register user_not_member, compulsory fields only (except checkboxes on page 4/5)", () => {
+  /*
+  beforeEach(): better practice than afterEach(). 
+  WHY: https://docs.cypress.io/guides/references/best-practices#Using-after-or-afterEach-hooks 
+  */
+  beforeEach(() => {
+    // authenticate as superuser
+    cy.request("POST", "http://collectivo.local:8000/api/dev/token/", {
+      username: "test_superuser@example.com",
+      password: "Test123!",
+    }).then((response) => {
+      const token = response.body.access_token;
+      const retrieveMembersParams = {
+        method: "GET",
+        url: `http://localhost:8000/api/members/members/?first_name__contains=%C3%96tz1&last_name__contains=CABBAG3&person_type=natural`,
+        headers: {
+          authorization: token,
+        },
+      };
+      // retrieve member(s) matching test_user_not_member sent registration from previous test execution
+      cy.request(retrieveMembersParams).then((response) => {
+        if (response.status === 200 && response.body.length > 0) {
+          for (let i = 0; i < response.body.length; i++) {
+            const member = response.body[i];
+            console.log("Deleting member with id: ", member.id);
+            const deleteMembersParameters = {
+              method: "DELETE",
+              url: `http://localhost:8000/api/members/members/${member.id}/`,
+              headers: {
+                authorization: token,
+              },
+            };
+            // delete member(s)
+            cy.request(deleteMembersParameters).then((response) =>
+              expect(response.status).to.eq(204)
+            );
+          }
+        } else {
+          console.log("No members found, continuing testCase...");
+        }
+      });
+    });
+  });
+  it("register as natural person", () => {
     cy.login("test_user_not_member@example.com");
     cy.get("div[id='members-registration-tile']").should("exist");
     cy.get("button[id='members-registration-tile-continue']").click();
@@ -48,25 +102,23 @@ describe("register user not member filling out only compulsory fields and checkb
     cy.get("#skills-input > .multipleChoice input").check("7");
     cy.get("#next-page-button > .button button").click();
     /* Page 5/5 */
+    /* 
+    Checkbox cannot be selected because of: https://stackoverflow.com/questions/47551639/access-element-whose-parent-is-hidden-cypress-io, workaround possible 
+    */
+    // cy.get('#public-use-approved-checkbox > .boolean input').check() // DEPRECATED;
+    // cy.get('#founding-event-checkbox > .boolean input').check();
 
-    /* Checkbox cannot be selected because of this -> https://stackoverflow.com/questions/47551639/access-element-whose-parent-is-hidden-cypress-io, workaround possible */
-
-    //cy.get('#public-use-approved-checkbox > .boolean input').check();
-    //cy.get('#founding-event-checkbox > .boolean input').check();
-
+    cy.intercept({ method: "POST", url: "**/register" }).as("new-member");
     cy.get("#submit-button > .button button").click();
-    cy.intercept("/api/members/register").as("new-member");
-    //cy.wait('@new-member').then(console.log)
-    //cy.wait('@new-user').should('have.property', 'response.statusCode', 201)
-
-    //TODO intercept response
-    //TODO validate response
-    //TODO delete user now member
-
-    /**
-     * 1. Fill out register form (NP, Organisation)
-     * 2. Assert db-data is correct -> cy.intercept()
-     * 3. Delete members/members/id -> cy.request [ADMIN-USER]
-     */
+    cy.wait("@new-member").then(({ request, response }) => {
+      console.log("Request sent to server: ", request.body);
+      console.log("Response sent back to client: ", response?.body);
+      expect(response?.statusCode).to.eq(201);
+      /* Here response.body validation */
+    });
+    cy.get("#welcome-member-span").should(
+      "contain.text",
+      "Willkommen auf der MILA Mitgliederplattform!"
+    );
   });
 });
