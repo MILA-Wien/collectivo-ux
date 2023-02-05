82a0bf7d161aaeb330d19415cc83ecc4fab2d8ac
Riccardo Wasserman <riccardo.wasserman@gmail.com>
2023-02-05 23:17:30 +0100

    (c) 06_register_member: - generic GET as superuser - it.skip for np - generic button validation - register as lp (c) registrationForm: unique ids for DOM element

--- cypress/e2e/06_register_member.cy.ts
+++ cypress/e2e/06_register_member.cy.ts
@@ -10,8 +10,9 @@ README && TODOs:
       Manipulation of expectedResponse properties necessary, i.e. they need to be made dynamic: "id" (i), "membership_start" (ii) etc.
   2b: validation of specific actualResponse properties only. Which ones?
 
-3. it("register as legal person", () => {...}
+3. it("register as legal person", () => {...} //DONE
 4. Use cy.fixture() to load test data -> https://docs.cypress.io/api/commands/fixture
+5. Use generic button validation
 */
 
 Cypress.Cookies.debug(true);
@@ -29,7 +30,7 @@ describe("register user_not_member, compulsory fields only (except checkboxes on
       const token = response.body.access_token;
       const retrieveMembersParams = {
         method: "GET",
-        url: `http://localhost:8000/api/members/members/?first_name__contains=%C3%96tz1&last_name__contains=CABBAG3&person_type=natural`,
+        url: `http://localhost:8000/api/members/members/?first_name__contains=%C3%96tz1&last_name__contains=CABBAG3`,
         headers: {
           authorization: token,
         },
@@ -58,10 +59,16 @@ describe("register user_not_member, compulsory fields only (except checkboxes on
       });
     });
   });
-  it("register as natural person", () => {
+  it.skip("register as natural person", () => {
     cy.login("test_user_not_member@example.com");
-    cy.get("div[id='members-registration-tile']").should("exist");
-    cy.get("button[id='members-registration-tile-continue']").click();
+    cy.get("span[id='welcome-member-span']").should(
+      "contain.text",
+      "Willkommen auf der MILA Mitgliederplattform!"
+    );
+    cy.get('.p-card-body button').then(($btn) => {
+      const txt = $btn.text()
+      expect(txt).to.eq("Weiter");
+    }).click();
     /* Page 1/5 */
     cy.get("div[id='welcome-paragraph']").should("exist");
     cy.get('[type="radio"]').check("natural");
@@ -96,12 +103,7 @@ describe("register user_not_member, compulsory fields only (except checkboxes on
     cy.get("#skills-input > .multipleChoice input").check("7");
     cy.get("#next-page4-button > .button button").click();
     /* Page 5/5 */
-    /* 
-    Checkbox cannot be selected because of: https://stackoverflow.com/questions/47551639/access-element-whose-parent-is-hidden-cypress-io, workaround possible 
-    */
-    // cy.get('#public-use-approved-checkbox > .boolean input').check() // DEPRECATED;
-    // cy.get('#founding-event-checkbox > .boolean input').check();
-
+    cy.get('#checkbox_statutes_approved > .boolean input').check({force: true});
     cy.intercept({ method: "POST", url: "**/register" }).as("new-member");
     cy.get("#submit-button > .button button").click();
     cy.wait("@new-member").then(({ request, response }) => {
@@ -112,7 +114,70 @@ describe("register user_not_member, compulsory fields only (except checkboxes on
     });
     cy.get("#welcome-member-span").should(
       "contain.text",
+      "CABBAG3"
+    );
+  });
+
+  it("register as legal person", () => {
+    cy.login("test_user_not_member@example.com");
+    cy.get("span[id ='welcome-member-span']").should(
+      "contain.text",
       "Willkommen auf der MILA Mitgliederplattform!"
+    );
+    cy.get('.p-card-body button').then(($btn) => {
+      const txt = $btn.text()
+      expect(txt).to.eq("Weiter");
+    }).click();
+    /* Page 1/5 */
+    cy.get("div[id='welcome-paragraph']").should("exist");
+    cy.get('[type="radio"]').check("legal");
+    cy.get("#next-page1-button > .button button").click();
+    /* Page 2/5 */
+
+    cy.get("#lp-name-input input").type("Ötzi CABBAG3");
+    cy.get("#lp-legal-type-input input").type("GG");
+    cy.get("#lp-identification-number-input input").type("000000x");
+    
+    cy.get("#lp-address-street-input input").click().type(
+      "ÄltGasseMitNötFünnyChars"
+    );
+    cy.get("#lp-address-street-number-input input").click().type("28392");
+    cy.get("#lp-address-postcode-input input").click().type("1111");
+    cy.get("#lp-address-city-input input").click().type("Weän");
+    cy.get("#lp-address-country-input input").click().type("Österland");
+    
+    cy.get("#lp-first-name-input input").clear().type("Ötz1");
+    cy.get("#lp-last-name-input input").clear().type("CABBAG3");
+    cy.get('[type="radio"]').check("male");
+    
+    cy.get("#next-page2-button > .button button").click();
+    /* Page 3/5 */
+    cy.get('[type="radio"]').check("normal");
+    cy.get('[type="radio"]').check("sepa");
+    cy.get("#bank-account-input input").type(
+      "AT592250039687965121"
+    );
+    cy.get("#bank-account-owner-input input").type("Ötz1 CABBAG3");
+    cy.get("#next-page3-button button").click();
+    /* Page 4/5 */
+    cy.get("#groups-interested-input input").check("1");
+    cy.get("#groups-interested-input input").check("9");
+    cy.get("#skills-input input").check("1");
+    cy.get("#skills-input input").check("7");
+    cy.get("#next-page4-button button").click();
+    /* Page 5/5 */
+    cy.get('#checkbox_statutes_approved input').check({force: true});
+    cy.intercept({ method: "POST", url: "**/register" }).as("new-member");
+    cy.get("#submit-button > .button button").click();
+    cy.wait("@new-member").then(({ request, response }) => {
+      console.log("Request sent to server: ", request.body);
+      console.log("Response sent back to client: ", response?.body);
+      expect(response?.statusCode).to.eq(201);
+      /* Here response.body validation */
+    });
+    cy.get("#welcome-member-span").should(
+      "contain.text",
+      "CABBAG3"
     );
   });
 });
