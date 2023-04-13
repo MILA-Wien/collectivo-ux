e4b56a9e9e76879c11d172a254800bbb455f10d0
Riccardo Wasserman <riccardo.wasserman@gmail.com>
2023-04-13 17:37:10 +0200

    (c) main.ts: remove commented out code

--- src/main.ts
+++ src/main.ts
@@ -23,25 +23,22 @@ app.use(createPinia());
 const keycloakInstance = initKeycloak();
 keycloakInstance
   .then(() => {
-    // loadLocaleMessages(i18n, "en");
-    /* loadLocaleMessages(i18n, "de").then(()=>{
-      setI18nLanguage(i18n, "de") */
-      loadLocaleMessages(i18n, "en");
-      loadLocaleMessages(i18n, "de");
-      setI18nLanguage(i18n, "de");
-      app.use(i18n);
-      // init PrimeVue
-      app.use(PrimeVue);
-      app.use(ToastService);
-      app.use(ConfirmationService);
-      app.directive("tooltip", PrimeTooltip);
+    loadLocaleMessages(i18n, "en");
+    loadLocaleMessages(i18n, "de");
+    setI18nLanguage(i18n, "de");
+    app.use(i18n);
 
-      // init view router
-      app.use(router);
-      //setTimeout(() => {  app.mount("#app"); }, 3000);
-      // Render app
-      app.mount("#app");
-    //})
+    // init PrimeVue
+    app.use(PrimeVue);
+    app.use(ToastService);
+    app.use(ConfirmationService);
+    app.directive("tooltip", PrimeTooltip);
+
+    // init view router
+    app.use(router);
+
+    // Render app
+    app.mount("#app");
   })
   .catch((e) => {
     console.log("error", e);
