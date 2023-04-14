9b6d5882b8903cb725ece89f06dbc2beaf0e4993
Riccardo Wasserman <riccardo.wasserman@gmail.com>
2023-04-14 11:05:53 +0200

    (c) 05_open_user_table.cy.ts: comment in

--- cypress/e2e/05_open_user_table.cy.ts
+++ cypress/e2e/05_open_user_table.cy.ts
@@ -5,11 +5,10 @@ describe("Login Succesfull", () => {
   it("visits the app root url", () => {
     cy.login("test_superuser@example.com");
     cy.get('button[id="toggle-sidebar-button"]').click();
-    // TODO: This finds Users in the CI E2E because the menu is in english
-    // cy.get("#admin_menu > div > div > div > a > div > span")
-    //   .contains("Benutzer*innen")
-    //   .click();
-    // cy.get("#collectivo-backdrop").click();
-    // cy.get("#core-users").should("exist");
+    cy.get("#admin_menu > div > div > div > a > div > span")
+      .contains("Benutzer*innen")
+      .click();
+    cy.get("#collectivo-backdrop").click();
+    cy.get("#core-users").should("exist");
   });
 });
