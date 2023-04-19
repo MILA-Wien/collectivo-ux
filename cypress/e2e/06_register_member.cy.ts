d64eff4316f3c8a2e79ceecf1613e3ed23beb180
Joël Foramitti <57440945+JoelForamitti@users.noreply.github.com>
2023-04-19 17:22:30 +0200

    Remove intro

--- cypress/e2e/06_register_member.cy.ts
+++ cypress/e2e/06_register_member.cy.ts
@@ -56,10 +56,6 @@ describe("register user_not_member", () => {
   });
   it("register as natural person", () => {
     cy.login("test_user_not_member@example.com");
-    cy.get("span[id='welcome-member-span']").should(
-      "contain.text",
-      "Willkommen auf der MILA Mitgliederplattform!"
-    );
     cy.get(".p-card-body button")
       .then(($btn) => {
         const txt = $btn.text();
@@ -134,10 +130,6 @@ describe("register user_not_member", () => {
 
   it("register as legal person", () => {
     cy.login("test_user_not_member@example.com");
-    cy.get("span[id ='welcome-member-span']").should(
-      "contain.text",
-      "Willkommen auf der MILA Mitgliederplattform!"
-    );
     cy.get(".p-card-body button")
       .then(($btn) => {
         const txt = $btn.text();
