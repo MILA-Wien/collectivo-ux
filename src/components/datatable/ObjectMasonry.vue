a0cb1b20936261558dd65065a798af56a5e05fc4
Joël Foramitti <57440945+JoelForamitti@users.noreply.github.com>
2023-06-13 15:49:36 +0200

    Change to vue-masonry-wall

--- src/components/datatable/ObjectMasonry.vue
+++ src/components/datatable/ObjectMasonry.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { VueFlexWaterfall } from "vue-flex-waterfall";
+import MasonryWall from "@yeger/vue-masonry-wall";
 import type { PropType } from "vue";
-
 defineProps({
   items: {
     type: Array as PropType<any[]>,
@@ -11,27 +10,9 @@ defineProps({
 </script>
 
 <template>
-  <VueFlexWaterfall
-    align-content="stretch"
-    class="w-full"
-    col="3"
-    col-spacing="10px"
-    :break-at="{ 1536: 2, 768: 1 }"
-  >
-    <div v-for="(item, i) in items" :key="i" class="waterfall-item mb-5">
+  <MasonryWall :items="items" :ssr-columns="1" :column-width="300" :gap="16">
+    <template #default="{ item, index }">
       <slot name="item" v-bind="{ item }"></slot>
-    </div>
-  </VueFlexWaterfall>
+    </template>
+  </MasonryWall>
 </template>
-
-<style scoped lang="scss">
-.waterfall-item {
-  width: 100%;
-  @media screen and (min-width: 768px) {
-    width: calc(50% - 9px);
-  }
-  @media screen and (min-width: 1536px) {
-    width: calc(33.333% - 6px);
-  }
-}
-</style>
