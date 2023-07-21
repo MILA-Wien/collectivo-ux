c632c1ba714c1752db5e6fa5f86800d0407846e9
Aaron <aaron@nanu-c.org>
2023-07-21 22:41:04 +0200

    Avoid potential errors with external components

--- src/ main.d.ts
+++ src/ main.d.ts
@@ -1,3 +1,9 @@
 // each microfrontend namespace must be defined here
 declare module "test_extension/*" {}
 declare module "flexmasonry/src/flexmasonry";
+declare module "a/*" {}
+declare module "b/*" {}
+declare module "d/*" {}
+declare module "e/*" {}
+declare module "f/*" {}
+declare module "g/*" {}
